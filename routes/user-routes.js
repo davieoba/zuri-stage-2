2c05f12c3f6b747bffe7cc25b5447e33142be19d
David Bodunrin <bodunrindavidbond@gmail.com>
2023-09-16 13:58:58 +0100

    add endpoint to fetch all users

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const userController = require('../controllers/user-controller')
 
 router.post('/', userController.createUser)
+router.get('/', userController.getUsers)
 
 router.get('/:user_id', userController.getUser)
 
